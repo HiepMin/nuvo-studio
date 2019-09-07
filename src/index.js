c9c8c5602db3d2ecbd6054a01554d8d60675d2dd
HiepMin <hiepminh130497@gmail.com>
2019-09-07 20:45:23 +0700

    setup app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+// core
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// components/containers
+import App from './containers/App';
+// libs
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Fragment>
+    <App />
+  </Fragment>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
